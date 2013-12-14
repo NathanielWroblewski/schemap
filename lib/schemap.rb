37aef7c0d41c55c66e2c9d9c453a4a437efa5052
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-12-14 02:34:48 -0800

    wip

--- lib/schemap.rb
+++ lib/schemap.rb
@@ -4,16 +4,6 @@ require 'rails'
 module Schemap
   class Schemap
     def self.generate_schema(db)
-      db.tables.map do |table|
-        {table =>
-          {attributes:
-             (db.columns(table).map(&:name)),
-           associations:
-              table.classify.constantize.reflections.map { |r|
-                r[1].table_name
-              }
-          }
-        }
       end
     end
   end
