2ec381c1edd0137f3b33c2b12790b2405237d94a
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-12-13 23:58:46 -0800

    wip

--- lib/schemap.rb
+++ lib/schemap.rb
@@ -1,5 +1,20 @@
 require "schemap/version"
 
 module Schemap
-
+  class Schemap
+    def self.generate_schema
+      db = ActiveRecord::Base.connection
+      db.tables.map do |table|
+        {table =>
+          {attributes:
+             (db.columns(table).map(&:name)),
+           associations:
+              table.classify.constantize.reflections.map { |r|
+                r[1].table_name
+              }
+          }
+        }
+      end
+    end
+  end
 end
--- spec/schemap_spec.rb
+++ spec/schemap_spec.rb
@@ -1,3 +1,32 @@
-require 'rspec'
+require 'spec_helper'
 require_relative '../lib/schemap'
 
+# describe Schemap, '#generate_schema'
+
+
+
+
+
+# default to default schema location
+# error if no schema
+
+
+# describe Schemap::SchemaParser, '.replicate' do
+#   it 'maps tables to associations and attributes' do
+
+#   end
+# end
+
+
+# db = ActiveRecord::Base.connection
+# db.tables.map do |table|
+#   {table =>
+#     {attributes:
+#        (db.columns(table).map(&:name)),
+#      associations:
+#         table.classify.constantize.reflections.map { |r|
+#           r[1].table_name
+#         }
+#     }
+#   }
+# end
